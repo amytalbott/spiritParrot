5f1bd8cdc8b09b7d07500bfe38b9f24d2ae24a99
amytalbott <atalbott28@gmail.com>
2018-07-11 12:59:57 -0400

    first commit

---
+++ js/buttonHover.js
@@ -0,0 +1,3 @@
+$("button").hover(function () {
+    $(this).toggleClass("hovered");
+});
---
+++ js/showParrots.js
@@ -0,0 +1,133 @@
+/*This programs make parrots appear when their corresponding month is clicked*/
+
+// Hide the parrots
+$('.showJanuary').hide();
+// When the button is pressed
+$('.januaryButton').on('click', function(){
+  // show the parrot
+$('.showJanuary').show();
+  // and hide the button
+$('.januaryButton').hide();
+
+});
+
+// Hide the parrots
+$('.showFebruary').hide();
+// When the button is pressed
+$('.februaryButton').on('click', function(){
+  // show the parrot
+$('.showFebruary').show();
+  // and hide the button
+$('.februaryButton').hide();
+
+});
+
+// Hide the parrots
+$('.showMarch').hide();
+// When the button is pressed
+$('.marchButton').on('click', function(){
+  // show the parrot
+$('.showMarch').show();
+  // and hide the button
+$('.marchButton').hide();
+
+});
+
+// Hide the parrots
+$('.showApril').hide();
+// When the button is pressed
+$('.aprilButton').on('click', function(){
+  // show the parrot
+$('.showApril').show();
+  // and hide the button
+$('.aprilButton').hide();
+
+});
+
+// Hide the parrots
+$('.showMay').hide();
+// When the button is pressed
+$('.mayButton').on('click', function(){
+  // show the parrot
+$('.showMay').show();
+  // and hide the button
+$('.mayButton').hide();
+
+});
+
+// Hide the parrots
+$('.showJune').hide();
+// When the button is pressed
+$('.juneButton').on('click', function(){
+  // show the parrot
+$('.showJune').show();
+  // and hide the button
+$('.juneButton').hide();
+
+});
+
+// Hide the parrots
+$('.showJuly').hide();
+// When the button is pressed
+$('.julyButton').on('click', function(){
+  // show the parrot
+$('.showJuly').show();
+  // and hide the button
+$('.julyButton').hide();
+
+});
+
+// Hide the parrots
+$('.showAugust').hide();
+// When the button is pressed
+$('.augustButton').on('click', function(){
+  // show the parrot
+$('.showAugust').show();
+  // and hide the button
+$('.augustButton').hide();
+
+});
+
+// Hide the parrots
+$('.showSeptember').hide();
+// When the button is pressed
+$('.septemberButton').on('click', function(){
+  // show the parrot
+$('.showSeptember').show();
+  // and hide the button
+$('.septemberButton').hide();
+
+});
+
+// Hide the parrots
+$('.showOctober').hide();
+// When the button is pressed
+$('.octoberButton').on('click', function(){
+  // show the parrot
+$('.showOctober').show();
+  // and hide the button
+$('.octoberButton').hide();
+
+});
+
+// Hide the parrots
+$('.showNovember').hide();
+// When the button is pressed
+$('.novemberButton').on('click', function(){
+  // show the parrot
+$('.showNovember').show();
+  // and hide the button
+$('.novemberButton').hide();
+
+});
+
+// Hide the parrots
+$('.showDecember').hide();
+// When the button is pressed
+$('.decemberButton').on('click', function(){
+  // show the parrot
+$('.showDecember').show();
+  // and hide the button
+$('.decemberButton').hide();
+
+});
